9a75a93d4cb0cf16a64501eb71fe434e6b37af29
sdrapinski <szymondrapinski@gmail.com>
2026-03-01 11:03:51 +0100

    zad1

--- zajecia1/1_neuronowe.py
+++ zajecia1/1_neuronowe.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# jaka jest minimalna siec by to policzic
+# jaka jest optymalna siec by to policzic
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 
 model = Sequential()
 model.add(Dense(1, activation='linear'))
-model.add(Dense(4, activation='relu'))
-model.add(Dense(4, activation='relu'))
-model.add(Dense(4, activation='linear'))
+model.add(Dense(8, activation='linear'))
 model.add(Dense(1, activation='linear'))
 
 model.compile(optimizer='rmsprop', loss='mse')
@@ -16,7 +16,7 @@ model.compile(optimizer='rmsprop', loss='mse')
 df = pd.read_csv('f-c.csv', usecols=[1, 2])
 print(df)
 
-result = model.fit(df.F, df.C, epochs=300, verbose=2)
+result = model.fit(df.F, df.C, epochs=1000, verbose=2)
 
 C_pred = model.predict(df.F)
 plt.scatter(df.F, df.C)
